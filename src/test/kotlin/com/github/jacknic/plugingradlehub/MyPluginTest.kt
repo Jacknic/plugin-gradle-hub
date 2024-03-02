7a4509899f8a9c248adfd45820e6c6d72a546916
GitHub Action <action@github.com>
2024-03-02 13:14:06 +0000

    Template cleanup

--- src/test/kotlin/com/github/jacknic/plugingradlehub/MyPluginTest.kt
+++ src/test/kotlin/com/github/jacknic/plugingradlehub/MyPluginTest.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.jacknic.plugingradlehub
 
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.openapi.components.service
@@ -6,7 +6,7 @@ import com.intellij.psi.xml.XmlFile
 import com.intellij.testFramework.TestDataPath
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import com.intellij.util.PsiErrorElementUtil
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.jacknic.plugingradlehub.services.MyProjectService
 
 @TestDataPath("\$CONTENT_ROOT/src/test/testData")
 class MyPluginTest : BasePlatformTestCase() {
